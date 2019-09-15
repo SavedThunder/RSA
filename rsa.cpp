53a5f7c46bce361287450417eab495fc370a8bfa
Kyle Bernard O'Connor <thekyleoconnor@gmail.com>
2019-09-15 16:14:10 -0400

    Added a basic layout for what the code will look like
    TODO:
    
    Finish primality function
    finish RSA encryption
    finish RSA decryption

--- rsa.cpp
+++ rsa.cpp
@@ -1,5 +1,56 @@
-def startrsa():
-    print("Please input a message you would like to encrypt")
-    message = input()
-    print("Please input a public key integer")
-    pubkey = input()
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isPrime(int num){
+    // Base case for numbers < 4
+    if(num <= 1){
+        return false;
+    }
+    if(n <= 3){
+        return true;
+    }
+
+    // If it is not 2 but even it is not prime
+    if(num%2 == 0){
+        return false;
+    }
+
+    int d = num - 1;
+    while(d % 2 == 0){
+        d = d/2;
+    }
+
+    for(int i = 0; i < 5; i++){
+        if(!millerrabin(d, num)){
+            return false;
+        }
+    }
+    return true;
+}
+
+int encryptRSA(string message, int pubkey, int privkeyW){
+    int encrypted = 0;
+    return encrypted;
+}
+
+string decrypt(int message, int pubkey, int privkey){
+    string message = "";
+    return message;
+}
+
+int main{
+    int pubkey = 0;
+    string message = "";
+    cout << "Please input a message" << endl;
+    cin >> message;
+    bool prime_flag = false;
+    while(!prime_flag){
+        cout << "Please enter a public key" << endl;
+        cin >> pubkey;
+        primeflag = isPrime(pubkey);
+    }
+
+
+}
